1e78a207445059e47c5a16a2c90ac86c1b8a9ec6
TFF <github-actions[bot]@users.noreply.github.com>
2025-09-24 19:19:21 +0200

    Make the join less flakey

--- link-raid-automation.py
+++ link-raid-automation.py
@@ -41,9 +41,9 @@ JOIN_TEAM = config.get("general", "join_team").replace(" ", "").lower()
 JOIN_DIFF = config.get("general", "join_diff")
 if "-" in JOIN_DIFF:
     start, end = map(int, JOIN_DIFF.split("-"))
-    LEVELS_TO_FIND = list(range(start, end + 1))
+    LEVELS_TO_FIND = list(map(str, range(start, end + 1)))
 else:
-    LEVELS_TO_FIND = [int(JOIN_DIFF)]
+    LEVELS_TO_FIND = [JOIN_DIFF]
 
 TARGET_WINDOW = config.get("general", "exe_name")
 MAX_SCROLL_ATTEMPTS = config.getint("general", "max_scroll_attempts")
@@ -56,15 +56,28 @@ def get_game_window():
     return wins[0]
 
 
-def confirm_desired_difficulty(data, levels):
+def get_data_in_img(cords: str):
+    img = ImageGrab.grab(text_locations[cords])
+    gray = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
+    gray = cv2.bitwise_not(gray)
+    data = pytesseract.image_to_data(gray, output_type=pytesseract.Output.DICT)
+
+    if DEBUG:
+        img.save(f"debug/{cords}.png")
+        Image.fromarray(gray).save(f"debug/{cords}_gray.png")
+        print(cords, ">", data["text"])
+    return data
+
+
+def get_text_in_img(cords: str) -> str:
+    data = get_data_in_img(cords)
+    return "".join(data["text"]).lower().replace(" ", "")
+
+
+def find_coords_for_difficulties():
+    data = get_data_in_img("battle_box")
     for i, text in enumerate(data["text"]):
-        if not text.strip():
-            continue
-        if (
-            "lvl" in text.lower()
-            and data["text"][i + 1].strip().isdigit()
-            and int(data["text"][i + 1]) in levels
-        ):
+        if "lvl" in text.lower() and data["text"][i + 1] in LEVELS_TO_FIND:
             x, y, w, h = (
                 data["left"][i],
                 data["top"][i],
@@ -72,17 +85,15 @@ def confirm_desired_difficulty(data, levels):
                 data["height"][i],
             )
             return x, y, w, h
+    return 0, 0, 0, 0
 
 
 def start_join():
     for attempt in range(MAX_SCROLL_ATTEMPTS):
         if DEBUG:
             print(f"Scroll attempt {attempt + 1:2}/{MAX_SCROLL_ATTEMPTS}")
-        data = get_img("battle_box")
-        match = confirm_desired_difficulty(data, LEVELS_TO_FIND)
-
-        if match:
-            x, y, w, h = match
+        x, y, w, h = find_coords_for_difficulties()
+        if x:
             pyautogui.click(
                 text_locations["battle_box"][0] + x + w // 2,
                 text_locations["battle_box"][1] + y + h // 2,
@@ -112,7 +123,8 @@ def start_join():
 class CurrentState(Enum):
     JOIN_SCREEN = "JOIN_SCREEN"
     HOST_SCREEN = "HOST_SCREEN"
-    HOME_SCREEN = "HOME_SCREEN"
+    HOME_SCREEN_CAN_HOST = "HOME_SCREEN_CAN_HOST"
+    HOME_SCREEN_CANNOT_HOST = "HOME_SCREEN_CANNOT_HOST"
     NO_ACTION = "NO_ACTION"
     RESULTS_SCREEN = "RESULTS_SCREEN"
     BACK_SCREEN = "BACK_SCREEN"
@@ -120,53 +132,36 @@ class CurrentState(Enum):
     PLAY_HOST_SCREEN = "PLAY_HOST_SCREEN"
 
 
-def get_img(cords: str):
-    img = ImageGrab.grab(text_locations[cords])
-    gray = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
-    gray = cv2.bitwise_not(gray)
-    data = pytesseract.image_to_data(gray, output_type=pytesseract.Output.DICT)
-
-    if DEBUG:
-        img.save(f"debug/{cords}.png")
-        Image.fromarray(gray).save(f"debug/{cords}_gray.png")
-        print(cords, ">", data["text"])
-    return data
-
-
 def current_state() -> CurrentState:
-    data = get_img("result_box")
-    for text in data["text"]:
-        if "lvl" in text.lower():
-            return CurrentState.RESULTS_SCREEN
+    text = get_text_in_img("result_box")
+    if "lvl" in text.lower():
+        return CurrentState.RESULTS_SCREEN
 
-    data = get_img("battle_box")
-    for text in data["text"]:
-        if "lvl" in text.lower():
-            return CurrentState.JOIN_SCREEN
+    text = get_text_in_img("join_button_box")
+    if "join" in text.lower():
+        return CurrentState.JOIN_SCREEN
 
-    data = get_img("host_box")
-    for text in data["text"]:
-        if "ready" in text.lower():
-            return CurrentState.HOST_SCREEN
+    text = get_text_in_img("host_box")
+    if "ready" in text.lower():
+        return CurrentState.HOST_SCREEN
 
-    data = get_img("like_box")
-    for text in data["text"]:
-        if text.isdigit():
-            return CurrentState.HOME_SCREEN
+    text = get_text_in_img("like_box")
+    if text.isdigit():
+        text2 = get_text_in_img("can_host_box")
+        if "play" in text.lower() and "0/6" not in text2:
+            return CurrentState.HOME_SCREEN_CAN_HOST
+        return CurrentState.HOME_SCREEN_CANNOT_HOST
 
-    data = get_img("back_box")
-    for text in data["text"]:
-        if "back" in text.lower():
-            return CurrentState.BACK_SCREEN
+    text = get_text_in_img("back_box")
+    if "back" in text.lower():
+        return CurrentState.BACK_SCREEN
 
-    data = get_img("party_box")
-    for text in data["text"]:
-        if "party" in text.lower():
-            data2 = get_img("play_box")
-            for text in data2["text"]:
-                if "play" in text.lower():
-                    return CurrentState.PLAY_JOIN_SCREEN
-            return CurrentState.PLAY_HOST_SCREEN
+    text = get_text_in_img("party_box")
+    if "party" in text.lower():
+        text2 = get_text_in_img("play_box")
+        if "play" in text2.lower():
+            return CurrentState.PLAY_JOIN_SCREEN
+        return CurrentState.PLAY_HOST_SCREEN
 
     return CurrentState.NO_ACTION
 
@@ -180,8 +175,9 @@ def setup_text_locations():
         win.activate()
     except Exception as e:
         print(
-            f'Could not activate window: {e}.\nThis is not a major issue, just be sure that no application is hiding Exedra from view, \n\
-The OCR has to "see" the content of the game to determine what to do.'
+            f"""Could not activate window: {e}.
+This is not a major issue, just be sure that no application is hiding Exedra from view,
+The OCR has to "see" the content of the game to determine what to do."""
         )
     text_locations["result_box"] = (
         win.left + 0.23 * win.width,
@@ -216,6 +212,12 @@ The OCR has to "see" the content of the game to determine what to do.'
     text_locations["join_button"] = (
         int(win.right - 0.15 * win.width),
         int(win.bottom - 0.18 * win.height),
+    )
+    text_locations["join_button_box"] = (
+        win.left + 0.8 * win.width,
+        win.top + 0.81 * win.height,
+        win.right - 0.12 * win.width,
+        win.bottom - 0.14 * win.height,
     )
     text_locations["play_button"] = (
         int(win.right - 0.4 * win.width),
@@ -259,13 +261,17 @@ The OCR has to "see" the content of the game to determine what to do.'
         win.right - 0.15 * win.width,
         win.bottom - 0.13 * win.height,
     )
+    text_locations["can_host_box"] = (
+        win.left + 0.32 * win.width,
+        win.top + 0.83 * win.height,
+        win.right - 0.57 * win.width,
+        win.bottom - 0.13 * win.height,
+    )
 
 
 def select_correct_team(team_name):
     for _ in range(10):
-        if team_name.lower() in "".join(get_img("team_name")["text"]).lower().replace(
-            " ", ""
-        ):
+        if team_name.lower() in get_text_in_img("team_name"):
             return
         pydirectinput.click(
             text_locations["next_team"][0], text_locations["next_team"][1]
@@ -279,8 +285,7 @@ def main():
     while True:
         pyautogui.sleep(1)
         state = current_state()
-        if DEBUG:
-            print("Current State:", state.name)
+        print("Current State:", state.name)
         match state:
             case CurrentState.JOIN_SCREEN:
                 start_join()
@@ -289,9 +294,12 @@ def main():
                     int(text_locations["host_box"][0]),
                     int(text_locations["host_box"][1]),
                 )
-            case CurrentState.HOME_SCREEN:
-                # TODO: Do host if not done hosting
-                # pydirectinput.click(text_locations["host_screen_button"][0], text_locations["host_screen_button"][1])
+            case CurrentState.HOME_SCREEN_CAN_HOST:
+                pydirectinput.click(
+                    text_locations["host_screen_button"][0],
+                    text_locations["host_screen_button"][1],
+                )
+            case CurrentState.HOME_SCREEN_CANNOT_HOST:
                 pydirectinput.click(
                     text_locations["join_screen_button"][0],
                     text_locations["join_screen_button"][1],
@@ -315,8 +323,8 @@ def main():
                 # No need to ping the process that much while in battle.
             case CurrentState.RESULTS_SCREEN:
                 pydirectinput.click(
-                    int(text_locations["result_box"][0]),
-                    int(text_locations["result_box"][1]),
+                    int(text_locations["result_box"][2]),
+                    int(text_locations["result_box"][3]),
                 )
             case CurrentState.BACK_SCREEN:
                 pydirectinput.click(
@@ -328,4 +336,5 @@ def main():
 
 if __name__ == "__main__":
     setup_text_locations()
+    print("starting with config:", dict(config["general"]))
     main()
